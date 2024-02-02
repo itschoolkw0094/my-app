751dbad5a52d0540e05556c4225f9b3272e6071a
itschoolkw0094 <itschoolkw0094@gmail.com>
2024-02-02 14:26:08 +0900

    added utils, contexts, etc.

---
+++ news-app/src/app/services/auth/signout.ts
@@ -0,0 +1,22 @@
+import type { ApiContext } from "@/app/types/data"
+import { fetcher } from "@/app/utils"
+
+/**
+ * 認証API（サインアウト）
+ * @param context APIコンテキスト
+ * @returns サインアウトメッセージ
+ */
+const signout = async (context: ApiContext): Promise<{ message: string }> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signout`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+}
+
+export default signout
