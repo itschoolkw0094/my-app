be6e55eb47d0d42b18631e5ebd4acf8d86a6df4b
itschoolkw0094 <itschoolkw0094@gmail.com>
2024-03-11 14:58:58 +0900

    commit 240311

--- news-app/src/components/mainAppUI/InputArea.tsx
+++ news-app/src/components/mainAppUI/InputArea.tsx
@@ -21,19 +21,24 @@ const InputArea = () => {
     console.log(data.content)
   }
 
-  const [isOpenOffcanvas, setIsOpenOffcanvas] = useState(false)
+  const [isOpenOffcanvas, setIsOpenOffcanvas] = useState<boolean>(false)
+
+  const openOffcanvas = () => {
+    const v = !isOpenOffcanvas
+    setIsOpenOffcanvas(v)
+    console.log(v)
+  }
 
   return (
-    <div>
-    {/* {isOpenOffcanvas ?
-    ( */}
-      <div id="hs-overlay-bottom" className={`hs-overlay hs-overlay-open:translate-y-0 translate-y-full transition-all duration-300 fixed bottom-0 inset-x-0 transform max-h-40 size-full z-[80] bg-white border-b dark:bg-gray-800 dark:border-gray-700 hidden`} tabIndex={-1}>
+    <>
+    {isOpenOffcanvas ? (
+      <div id="" className={`hs-overlay hs-overlay-open:translate-y-0 translate-y-full transition-all duration-300 fixed bottom-0 inset-x-0 transform max-h-40 size-full z-[80] bg-white border-b dark:bg-gray-800 dark:border-gray-700`} tabIndex={-1}>
   <div className="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
     <h3 className="font-bold text-gray-800 dark:text-white">
       Offcanvas title
     </h3>
     <button 
-    // onClick={() => openOffcanvas()}
+    onClick={() => openOffcanvas()}
     type="button" className="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
       data-hs-overlay="#hs-overlay-bottom"
     >
@@ -47,25 +52,24 @@ const InputArea = () => {
     </p>
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" {...register('content', { required: true })}></input>
-      <button type="submit" data-hs-overlay="#hs-overlay-bottom">send</button>
+      <button type="submit" data-hs-overlay="">send</button>
     </form>
   </div>
 </div>
-    {/* ):
-    ( */}
+    ):
+    (
       <div className="fixed bottom-0 inset-x-0 transform w-full items-center bg-white">
         <button
           type="button" 
           className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          // onClick={openOffcanvas}
-          data-hs-overlay="#hs-overlay-bottom"
+          onClick={() => openOffcanvas()}
+          // data-hs-overlay="#hs-overlay-bottom"
           >
           Comment for this areticle !
         </button>
       </div>
-    {/* )} */}
-
-    </div>
+    )}
+    </>
   )
 }
 
