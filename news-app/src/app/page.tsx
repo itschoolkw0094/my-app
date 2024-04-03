cfa646ac62c3eb2d1fd3f88bbdd3b967adcdfd24
itschoolkw0094 <itschoolkw0094@gmail.com>
2024-04-03 10:55:19 +0900

    fixed root-page

--- news-app/src/app/page.tsx
+++ news-app/src/app/page.tsx
@@ -10,54 +10,9 @@ import SideBar from "@/components/Nav/SideBar";
 import InputArea from "@/components/mainAppUI/InputArea";
 
 export default function Home() {
-  const [articles, setArticles] = useState<ArticleType[]>([]);
-  const [prosComment, setProsComment] = useState<CommentType[]>([]);
-  const [consComment, setConsComment] = useState<CommentType[]>([]);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      const resArticle = await fetch("/api/news/getnews", {
-        method: "GET",
-      });
-      const resultArticle = await resArticle.json();
-      await setArticles(resultArticle);
-    };
-    fetchNews();
-  }, []);
-
-  const fetchComments = async () => {
-    if (articles[0] === undefined) return;
-
-    const prosParams = {
-      newsId: articles[0].id,
-      type: "true",
-    };
-    const prosQuery = new URLSearchParams(prosParams);
-    const consParams = {
-      newsId: articles[0].id,
-      type: "false",
-    };
-    const consQuery = new URLSearchParams(consParams);
-    const resProsComment = await fetch(
-      `http://localhost:3000/api/comment/getcomment?${prosQuery}`
-    );
-    const resConsComment = await fetch(
-      `http://localhost:3000/api/comment/getcomment?${consQuery}`
-    );
-
-    const resultProsComment = await resProsComment.json();
-    const resultConsComment = await resConsComment.json();
-
-    setProsComment(resultProsComment);
-    setConsComment(resultConsComment);
-  };
-  fetchComments();
-
   return (
-    <main className="flex flex-col w-full max-w-3xl mx-auto p-2">
-      <ArticleCard article={articles[0]} />
-      <CommentTab prosComments={prosComment} consComments={consComment} />
-      <InputArea />
-    </main>
+    <>
+      <h1>test</h1>
+    </>
   );
 }
