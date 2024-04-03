a925977f7d5fbcd71eaa67b5e72d477b8ab7c3c8
itschoolkw0094 <itschoolkw0094@gmail.com>
2024-04-03 10:58:44 +0900

    deleted utils

--- news-app/src/app/page.tsx
+++ news-app/src/app/page.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import { useAuthGuard } from "@/utils/hooks";
-import { signOut, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import CommentTab from "@/components/mainAppUI/CommentTab";
-import ArticleCard from "@/components/mainAppUI/ArticleCard";
-import { ArticleType, CommentType } from "@/types/data";
-import SideBar from "@/components/Nav/SideBar";
-import InputArea from "@/components/mainAppUI/InputArea";
-
 export default function Home() {
   return (
     <>
